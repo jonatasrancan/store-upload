1b34438bacd4914b6c1e205452e4ae1b3f3ffac2
Jônatas Rancan de Souza <jonatasrancan@gmail.com>
2018-10-10 14:28:30 -0300

    Store tenant on local storage and passes it on HTTP header on API calls

--- src/scripts/services/auth.js
+++ src/scripts/services/auth.js
@@ -4,6 +4,8 @@ import AUTH_CONFIG from "../../config/auth0-variables";
 import UserActions from "../actions/user-actions";
 import ErrorsActions from "../components/errors/errors-actions";
 
+const tenantNameSpace = 'https://myauth0.com/tenant';
+
 export default class Auth {
   constructor() {
     this.auth0 = new auth0.WebAuth({
@@ -44,6 +46,7 @@ export default class Auth {
         localStorage.setItem("access_token", authResult.accessToken);
         localStorage.setItem("id_token", authResult.idToken);
         localStorage.setItem("expires_at", expiresAt);
+        localStorage.setItem("tenant", authResult.idTokenPayload[tenantNameSpace])
       }
       UserActions.loggedIn(authResult.accessToken, authResult.idToken);
     }
@@ -54,6 +57,7 @@ export default class Auth {
       localStorage.removeItem("access_token");
       localStorage.removeItem("id_token");
       localStorage.removeItem("expires_at");
+      localStorage.removeItem("tenant");
     }
     this.auth0.logout();
     // UserActions.loggedOut();
@@ -74,6 +78,17 @@ export default class Auth {
         ErrorsActions.error("No access token found");
       }
       return accessToken;
+    }
+    return undefined;
+  }
+
+  getTenantName() {
+    if (window.localStorage) {
+      const tenantName = localStorage.getItem("tenant");
+      if (!tenantName) {
+        ErrorsActions.error("No tenant found");
+      }
+      return tenantName;
     }
     return undefined;
   }
